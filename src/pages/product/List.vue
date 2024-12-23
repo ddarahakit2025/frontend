6659b0ef4472a4b73ef966cc523c25d48b8aee34
ddarahakit2024 <ddarahakit2024@gmail.com>
2024-12-23 16:06:15 +0900

    [Feat] 상품 목록 기능에 HTML, CSS 적용

--- src/pages/product/List.vue
+++ src/pages/product/List.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue'
 import { useProductStore } from '../../stores/useProductStore'
 import { useLoadingStore } from '../../stores/useLoadingStore'
+import ProductCard from './ProductCard.vue';
 
 const loadingStore = useLoadingStore()
 const productStore = useProductStore()
@@ -79,46 +80,8 @@ onMounted(async () => {
                 <div class="spinner"></div>
             </div>
             <ul v-else class="product-list">
-                <li class="product-list-item" v-for="product in productStore.products" :key="product.id">
-                    <div class="product-list-item-wrapper">
-                        <a href="/detail.html">
-                            <div class="product-image-wrapper">
-                                <img src="https://img.29cm.co.kr/item/202310/11ee62764f2aec1da5b337e05227e995.jpg?width=400"
-                                    alt="[유리하다PICK] GRACIELA premium wool handmade coat_charcoal" />
-                            </div>
-                        </a>
-                        <div class="product-info-wrapper">
-                            <a href="/detail.html" class="product-info-link">르니나</a><a href="/detail.html">
-                                <div>
-                                    <h5 class="product-name">
-                                        [유리하다PICK] GRACIELA premium wool handmade
-                                        coat_charcoal
-                                    </h5>
-                                    <div class="product-sale-price-wrapper">
-                                        <strong class="product-sale-price">468,000</strong>
-                                        <div class="product-price-wrapper">
-                                            <span class="product-sale">24%</span><strong
-                                                class="product-price">355,680</strong>
-                                        </div>
-                                    </div>
-                                    <ul class="product-card">
-                                        <li color="#1d1d1d" class="product-card-item">쿠폰</li>
-                                    </ul>
-                                </div>
-                            </a>
-                            <div class="product-bottom-wrapper">
-                                <button class="product-like">
-                                    <i class="fa-regular fa-heart"></i> 2,463
-                                </button>
-                                <a href="https://product.29cm.co.kr/catalog/2270129#review" class="product-review">
-                                    <span class="review">
-                                        <i class="fa-regular fa-comment"></i>48
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                <ProductCard v-for="product in productStore.products" :key="product.id" :product="product">
+                </ProductCard>
             </ul>
         </div>
     </div>
---
+++ src/pages/product/ProductCard.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    product: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <li class="product-list-item">
+        <div class="product-list-item-wrapper">
+            <a href="/detail.html">
+                <div class="product-image-wrapper">
+                    <img src="https://img.29cm.co.kr/item/202310/11ee62764f2aec1da5b337e05227e995.jpg?width=400"
+                        :alt="product.name" />
+                </div>
+            </a>
+            <div class="product-info-wrapper">
+                <a href="/detail.html" class="product-info-link">{{ product.seller }}</a><a href="/detail.html">
+                    <div>
+                        <h5 class="product-name">
+                            {{ product.name }}
+                        </h5>
+                        <div class="product-sale-price-wrapper">
+                            <strong class="product-sale-price">{{ product.originalPrice }}</strong>
+                            <div class="product-price-wrapper">
+                                <span class="product-sale">24%</span><strong class="product-price">
+                                    {{ product.price }}
+                                </strong>
+                            </div>
+                        </div>
+                        <ul class="product-card">
+                            <li color="#1d1d1d" class="product-card-item">쿠폰</li>
+                        </ul>
+                    </div>
+                </a>
+                <div class="product-bottom-wrapper">
+                    <button class="product-like">
+                        <i class="fa-regular fa-heart"></i> 2,463
+                    </button>
+                    <a href="https://product.29cm.co.kr/catalog/2270129#review" class="product-review">
+                        <span class="review">
+                            <i class="fa-regular fa-comment"></i>48
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </li>
+</template>
+
+<style scoped></style>
